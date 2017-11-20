c956fcfcdf6d0eae00281e270341d6560d17de87
huaan0214 <1097634923@qq.com>
2017-11-20 23:38:41 +0800

    update

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-		url(r'^$',views.index),
+		url(r'^$',views.index_blog,name="home"),
 		url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_index'),
-		url(r'^edit\/*$',views.edit_page,name='edit_page'),
+		url(r'^edit/(?P<article_id>[0-9]+)$',views.edit_page,name='edit_page'),
 		url(r'^edit/action\/*$',views.edit_action,name='edit_action'),
+		url(r'^delete/(?P<article_id>[0-9]+)$',views.delete_page,name='delete_page'),
+		
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from . import models
 
 	
 # Create your views here.
-def index(request):#参数严格意义都可以
+def index_blog(request):#参数严格意义都可以
 	articles=models.Article.objects.all()
 	return render(request,'blog/index.html',{'articles':articles})
 
@@ -12,12 +12,31 @@ def article_page(request,article_id):
 	article = models.Article.objects.get(pk=article_id)
 	return render(request,'blog/articlePage.html',{'article':article})
 		
-def edit_page(request):
-	return render(request,'blog/editPage.html')
+def edit_page(request,article_id):
+	if str(article_id)=='0':
+		return render(request,'blog/editPage.html')
+	else:
+		article=models.Article.objects.get(pk=article_id)
+		return render(request,'blog/editPage.html',{'article':article})
 
 def edit_action(request):
-	title = request.POST.get('title','TITLE')
-	content = request.POST.get('content','CONTENT')
-	models.Article.objects.create(title=title,content=content)
+	title = request.POST.get('title')
+	content = request.POST.get('content')
+	article_id =request.POST.get('article_id','0')#defaulr :'0'
+	if article_id =='0':
+		models.Article.objects.create(title=title,content=content)
+		return HttpResponseRedirect(reverse('blog:home'))
+		#articles=models.Article.objects.all()
+		#return render(request,'blog/index.html',{'articles':articles})
+	else:
+		article=models.Article.objects.get(pk=article_id)
+		article.title=title
+		article.content=content
+		article.save()
+		return render(request,'blog/articlePage.html',{'article':article})
+			
+def delete_page(request,article_id):
+	models.Article.objects.get(pk=article_id).delete()
 	articles=models.Article.objects.all()
 	return render(request,'blog/index.html',{'articles':articles})
+	
