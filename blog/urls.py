1b49fd44846b0695b53810b5b1ac923d68795edb
huaan0214 <1097634923@qq.com>
2017-11-20 16:17:33 +0800

    update

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-		url(r'^1\/*$',views.index),
+		url(r'^$',views.index),
+		url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_index'),
+		url(r'^edit\/*$',views.edit_page,name='edit_page'),
+		url(r'^edit/action\/*$',views.edit_action,name='edit_action'),
 ]
